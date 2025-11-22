e7ff476eb9bf931f7e67e862c5d49870bca49667
alexpereira2014 <alexpereira2004@gmail.com>
2025-11-22 19:31:52 -0300

    #1 Commit inicial

---
+++ src/main/java/br/com/lunacom/ivmmonitorprocessamento/IvmMonitorProcessamentoApplication.java
@@ -0,0 +1,13 @@
+package br.com.lunacom.ivmmonitorprocessamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IvmMonitorProcessamentoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IvmMonitorProcessamentoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/lunacom/ivmmonitorprocessamento/IvmMonitorProcessamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lunacom.ivmmonitorprocessamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IvmMonitorProcessamentoApplicationTests {
+
+//    @Test
+//    void contextLoads() {
+//    }
+
+}
