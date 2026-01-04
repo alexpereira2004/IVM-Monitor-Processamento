5fa87e863262bf1087eef23e7193c513797a5c07
alexpereira2014 <alexpereira2004@gmail.com>
2026-01-03 22:41:37 -0300

    #3 Uma unica query pode resolver

--- src/main/java/br/com/lunacom/ivmmonitorprocessamento/repository/MovimentoVendaRepository.java
+++ src/main/java/br/com/lunacom/ivmmonitorprocessamento/repository/MovimentoVendaRepository.java
@@ -1,8 +1,11 @@
 package br.com.lunacom.ivmmonitorprocessamento.repository;
 
 import br.com.lunacom.comum.domain.MovimentoVenda;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
@@ -12,9 +15,16 @@ import java.util.Optional;
 @Repository
 public interface MovimentoVendaRepository extends JpaRepository<MovimentoVenda, Integer>, JpaSpecificationExecutor<MovimentoVenda> {
 
-    List<MovimentoVenda> findAllByAtivoCodigoAndDataVendaAfter(String ativoCodigo, LocalDate dataInicial);
-
-    List<MovimentoVenda> findAllByAtivoCodigo(String ativoCodigo);
+    @Query("SELECT m FROM MovimentoVenda m " +
+            "WHERE m.ativo.codigo = :codigo " +
+            "AND (:excluirPrejuizos = 'N' OR m.precoPago < m.precoVenda)" +
+            "AND (:dataInicial IS NULL OR m.dataVenda > :dataInicial)" +
+            "ORDER BY m.dataVenda DESC")
+    List<MovimentoVenda> buscarUltimasCincoVendas(
+            @Param("codigo") String codigo,
+            @Param("dataInicial") LocalDate dataInicial,
+            @Param("excluirPrejuizos") String excluirPrejuizos,
+            Pageable pageable);
 
     Optional<MovimentoVenda> findTopByAtivoCodigoOrderByDataVendaDesc(String ativoCodigo);
 
